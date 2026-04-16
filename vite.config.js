50aa70d184752d144df5c52763902b8360fda449
Joan Faith <jfglifetime@gmail.com>
2026-04-17 06:28:17 +0800

    edited vite.config.js and hide the define section from it

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  /*
   define: {
     'import.meta.env.VITE_WEATHER_API_KEY': JSON.stringify(process.env.VITE_WEATHER_API_KEY)
-  }
+  }*/
 })
